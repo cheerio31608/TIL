dd6e56a44d67699f3ca2cd01d5cdd30800f1ee25
KIMSHINWOO <swkim9985@naver.com>
2019-05-12 17:48:46 +0900

    Add files via upload

---
+++ C# practice/단축키/dllTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_CreateDLL
+{
+    public class dllTest
+    {
+        public int fn_Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ C# practice/키보드 입력/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo keys;
+            do
+            {
+                if (Console.KeyAvailable)
+                {
+                    keys = Console.ReadKey(true);
+                    switch (keys.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            Console.WriteLine("윗방향키 입력됨");
+                            break;
+                        case ConsoleKey.DownArrow:
+                            Console.WriteLine("아랫방향키 입력됨");
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            Console.WriteLine("왼쪽방향키 입력됨");
+                            break;
+                        case ConsoleKey.RightArrow:
+                            Console.WriteLine("오른쪽방향키 입력됨");
+                            break;
+                        case ConsoleKey.Backspace:
+                            Console.WriteLine("백스페이스 입력됨");
+                            break;
+                        case ConsoleKey.B:
+                            Console.WriteLine("B키 입력됨");
+                            break;
+                    }
+                }
+            } while (true);
+        }
+    }
+}
